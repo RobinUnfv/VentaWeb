760e176eb7349a2e62263c6cfe7c998715b093a2
robinzon <2010001659@unfv.edu.pe>
2024-06-13 22:12:41 -0500

    Inicio del proyecto

---
+++ src/main/java/com/robin/ventaweb/VentawebApplication.java
@@ -0,0 +1,13 @@
+package com.robin.ventaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentawebApplication.class, args);
+	}
+
+}
